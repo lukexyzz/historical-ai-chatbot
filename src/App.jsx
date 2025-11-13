126d2afdb0b22272bb02734a72acccdff533a992
E T <ellis.trickett@gmail.com>
2025-11-13 17:10:44 +0000

    feat: Add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,33 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
-import LoginForm from './components/LoginForm.jsx'
+import Login from './pages/Login.jsx'
 
-function App() {
-  const [response, setResponse] = useState(null);
+export default function App() {
+//   const [response, setResponse] = useState(null);
 
 
-useEffect(() => {
-  fetch('http://localhost:3000/helloworld')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      setResponse(data.hello);
-    })
-    .catch(error => {
-      console.error('Fetch error:', error);
-    });
-}, []);
+// useEffect(() => {
+//   fetch('http://localhost:3000/helloworld')
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`);
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       setResponse(data.hello);
+//     })
+//     .catch(error => {
+//       console.error('Fetch error:', error);
+//     });
+// }, []);
 
   return (
-    <>
-      <div>
-        {response}
-      </div>
-      <LoginForm />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
