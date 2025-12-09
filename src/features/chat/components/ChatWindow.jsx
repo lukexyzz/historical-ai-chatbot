234716f71990812add570579723c8ba10a697913
E T <ellis.trickett@gmail.com>
2025-12-09 12:10:45 +0000

    bugfix: clear chat now sets mode and dialogueTree to null

--- src/features/chat/components/ChatWindow.jsx
+++ src/features/chat/components/ChatWindow.jsx
@@ -40,7 +40,9 @@ export default function ChatWindow({
     const handleClearChat = () => {
         setChat(prevChat => ({
             ...prevChat,
-            messages: []
+            messages: [],
+            mode: null,
+            dialogueTree: null
         }));
     };
 
